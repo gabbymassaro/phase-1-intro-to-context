042a602724180ce6704ada5d28344d7a70e929f6
Gabriella Massaro <gmassaro807@gmail.com>
2024-06-03 07:46:18 -0400

    WagesEarnedOnDate passes

--- index.js
+++ index.js
@@ -25,7 +25,6 @@ function createTimeInEvent(employeeRecord, dateStamp) {
     hour: parseInt(dateStamp.slice(11,13) + "00"),
     date: dateStamp.slice(0,10)
   }
-
   employeeRecord.timeInEvents.push(timeIn)
   return employeeRecord
 }
@@ -36,28 +35,32 @@ function createTimeOutEvent(employeeRecord, dateStamp) {
     hour: parseInt(dateStamp.slice(11,13) + "00"),
     date: dateStamp.slice(0,10)
   }
-
   employeeRecord.timeOutEvents.push(timeOut)
   return employeeRecord
 }
 
 function hoursWorkedOnDate (employeeRecord, date) {
-
   const startTimeInEvent = employeeRecord.timeInEvents.find(timeInEvent => timeInEvent.date === date);
   const endTimeOutEvent = employeeRecord.timeOutEvents.find(timeOutEvent => timeOutEvent.date === date);
-
   const startTime = startTimeInEvent.hour
   const endTime = endTimeOutEvent.hour
-
   const hoursWorked = (Math.abs(startTime - endTime) / 100)
   return hoursWorked
+}
+
+function wagesEarnedOnDate (employeeRecord, date) {
+  const payRate = employee.payPerHour
+  const wages = (employeeRecord * payRate)
+  return wages
 }
 
 
 
 
-const employee = createEmployeeRecord(["Gabby", "Romanowski", "Dev.", 3])
-const timeInEmployee = createTimeInEvent(employee, "2024-07-07 1230")
-const timeOutEmployee = createTimeOutEvent(employee, "2024-07-07 0530")
-// hoursWorkedOnDate(timeOutEmployee,"2024-07-07")
-// hoursWorkedOnDate(timeOutEmployee,"2024-07-09")
+
+
+const employee = createEmployeeRecord(["Julius", "Caesar", "General", 27])
+const timeInEmployee = createTimeInEvent(employee, "0044-03-15 0900")
+const timeOutEmployee = createTimeOutEvent(employee, "0044-03-15 1100")
+const hoursWorked = hoursWorkedOnDate(timeOutEmployee,"0044-03-15")
+const wagesEarnedOn = wagesEarnedOnDate(hoursWorked,"0044-03-15")
