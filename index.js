5019b68d44635af8d2740015d2adfad5ef6e3bfd
Gabriella Massaro <gmassaro807@gmail.com>
2024-06-02 20:25:58 -0400

    CreateTimeInEvent tests pass

--- index.js
+++ index.js
@@ -7,17 +7,30 @@ function createEmployeeRecord(employee) {
     timeInEvents: [],
     timeOutEvents: []
   }
-
   return obj
 }
 
 function createEmployeeRecords(employeeData) {
   const employeeRecords = []
-
   employeeData.forEach(employee => {
     const record = createEmployeeRecord(employee)
     employeeRecords.push(record)
   })
-
   return employeeRecords
 }
+
+function createTimeInEvent(employeeRecord, dateStamp) {
+  const timeIn = {
+    type: "TimeIn",
+    hour: parseInt(dateStamp.slice(11,13) + "00"),
+    date: dateStamp.slice(0,10)
+  }
+
+  employeeRecord.timeInEvents.push(timeIn)
+  return employeeRecord
+}
+
+
+const employee = createEmployeeRecord(["Gabby", "", "", 3])
+const updatedEmployee = createTimeInEvent(employee, "2024-07-07 1230")
+console.log(updatedEmployee)
